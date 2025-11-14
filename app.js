9ba9a06b0f8cfa066dbeb84c5920c54bcde36ad6
HomamZituni <homam93@msn.com>
2025-11-14 11:32:32 -0500

    fixed html error and added global array + defined DOM elements

---
+++ app.js
@@ -0,0 +1,14 @@
+// Global array for posts to act as database for project
+let posts = [];
+
+// Define Elements for DOM
+
+const formElement = document.getElementById("post-form")
+const titleElement = document.getElementById("title")
+const contentElement = document.getElementById("content")
+const titleErrorElement = document.getElementById("title-error")
+const contentErrorElement = document.getElementById("content-error")
+const postsContainerElement = document.getElementById("posts-container")
+
+
+
